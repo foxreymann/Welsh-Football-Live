a5ad827e76bd6d05899c1470ef2cf78084c82d60
tomaszjureczko <tomasz.jureczko@gmail.com>
2011-11-20 18:12:14 +0000

    cool

--- assets/www/a.js
+++ assets/www/a.js
@@ -1,17 +1,9 @@
 function onBodyLoad() {
     $.ajax({
         url: 'http://www.pita.it/soccer/resultScores-wales.xml', // name of file you want to parse
-url: 'resultScores-wales.xml', // name of file you want to parse
+//url: 'resultScores-wales.xml', // name of file you want to parse
         dataType: "xml", // type of file you are trying to read
         success: parse, // name of the function to call upon success
-        error: function(){alert("Error: Something went wrong");},
-    });
-
-    $.ajax({
-        url: 'http://www.pita.it/soccer/resultScores-wales.xml', // name of file you want to parse
-url: 'LlanelliBalaTown.html', // name of file you want to parse
-        dataType: "html", // type of file you are trying to read
-        success: parse2, // name of the function to call upon success
         error: function(){alert("Error: Something went wrong");},
     });
 }
@@ -21,13 +13,27 @@ function parse(document){
   $(document).find("item").each(function(){
         console.log(this);
       $("#gamesList").prepend(
-          '<li data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c"><div class="ui-btn-inner ui-li" aria-hidden="true"><div class="ui-btn-text"><a href="#two" class="ui-link-inherit"><h3 class="ui-li-heading">'+$(this).find('title').text()+
+          '<li onclick="showGame(\''+ $(this).find('link').text() + '\',\'' + $(this).find('title').text() +'\')"   data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c"><div class="ui-btn-inner ui-li" aria-hidden="true"><div class="ui-btn-text"><a href="#two" class="ui-link-inherit"><h3 class="ui-li-heading">'+$(this).find('title').text()+
           '</h3><p class="ui-li-desc">'+$(this).find('description').text()+
           '</p></a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow"></span></div></li>'
       );
   });
 }
 
+function showGame(url,title) {
+    $('#two h1').text(title); 
+    $.ajax({
+        url: url,
+        dataType: "html",
+        success: parse2,
+        error: function(){alert("Error: Something went wrong");},
+    });
+}
+
 function parse2(document){
-  console.log(document);
+    for(x in $(document)) {
+        if(($(document)[x]).id === 'matchInfos') {
+            $('#gameInfo').html($(document)[x]);    
+        };
+    } 
 }
